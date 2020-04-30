2adbf053a7897dae82e1695532ff31fd73054a61
René Pohland <rene@webograph.de>
2020-04-30 19:41:31 +0200

    documentation for NewsScraper class

--- src/NewsScraper.ts
+++ src/NewsScraper.ts
@@ -6,8 +6,19 @@ export default class NewsScraper {
 
   constructor(private config: any) {}
 
+  /**
+   * Fetchs a page and returns parsing data
+   *
+   * @private
+   * @param {string} type
+   * @param {string} [url]
+   * @returns {Promise<any>}
+   * @memberof NewsScraper
+   */
   private async getData(type: string, url?: string): Promise<any> {
     if (!this.config.resolvers[type]) throw new Error(`No resolvers for the type ${type}`);
+
+    // ToDo: remove after pagination is added
     const sourceURL: string = url || this.config.base_url;
 
     try {
@@ -19,14 +30,34 @@ export default class NewsScraper {
     }
   }
 
+  /**
+   * Get the next list page data
+   *
+   * @returns {Promise<any>}
+   * @memberof NewsScraper
+   */
   public async getNextPage(): Promise<any> {
+     // ToDo: adding logic to paginate list pages
     return this.getData('list');
   }
 
+
+  /**
+   *  Get the data for a single article
+   *
+   * @param {string} url
+   * @returns {Promise<any>}
+   * @memberof NewsScraper
+   */
   public async getSingle(url: string): Promise<any> {
     return this.getData('single', url);
   }
 
+  /**
+   * Reset the pagination process
+   *
+   * @memberof NewsScraper
+   */
   public resetPagination(): void {
     this.current_page = 0;
   }
